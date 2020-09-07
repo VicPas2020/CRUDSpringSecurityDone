4ee6a715e36932c7493aa285c886f29eadc1f695
Kirill1504 <kirik072007@gmail.com>
2020-09-07 13:13:59 +0300

    commit5

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,69 +1,44 @@
 package web.dao;
 
-import org.hibernate.annotations.SelectBeforeUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao{
-    private EntityManagerFactory entityManagerFactory;
 
-    @Autowired
-    public void setEm(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
-    }
+    @PersistenceContext
+    private EntityManager em;
 
     @Override
     public List<User> listUsers() {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();
         List<User> list = em.createQuery("from User").getResultList();
-        em.getTransaction().commit();
-        em.close();
+
         return list;
     }
 
     @Override
     public void add(User user) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.getTransaction().begin();
         em.persist(user);
-        em.getTransaction().commit();
-        em.close();
     }
 
     @Override
     public void delete(User user) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-
-        em.getTransaction().begin();
         em.remove(em.contains(user) ? user : em.merge(user));
-        em.getTransaction().commit();
-        em.close();
     }
 
     @Override
     public void edit(User user) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-
-        em.getTransaction().begin();
         em.merge(user);
-        em.getTransaction().commit();
-        em.close();
     }
 
     @Override
     public User getById(int id) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-
-        em.getTransaction().begin();
         User user = em.find(User.class, id);
-        em.getTransaction().commit();
-        em.close();
 
         return user;
     }
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -42,7 +42,6 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    @Transactional
     public User getById(int id) {
         return userDao.getById(id);
     }
